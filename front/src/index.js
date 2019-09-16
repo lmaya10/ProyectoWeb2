fbc54f45d40a0379aeced83b53e56679fa275cd5
lmaya10 <l.maya10@uniandes.edu.co>
2019-09-15 22:22:08 -0500

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
+/* No entiendo porque importan App de App.js si la clase ahi se llama eventos
+
 ReactDOM.render(<App />,
   document.getElementById("root"));
